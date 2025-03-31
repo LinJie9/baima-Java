5c46a424fdd2e7616aebc376f095eed45e4931f4
LinJie9 <2983606672@qq.com>
2025-03-31 11:15:17 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Set<Integer> set = new TreeSet<>((a, b) -> b - a); //比较，从大到小
+//        set.add(1);
+//        set.add(2);
+//        set.add(3);
+//        for (Integer i : set){
+//            System.out.println(i);
+//        }
+
+//            Set<String> set = new LinkedHashSet<>(); //顺序排序
+//            set.add("AAA");
+//            set.add("CCC");
+//            set.add("DDD");
+//            for (String s : set){
+//                System.out.println(s);
+//            }
+
+
+//        LinkedList<String> list = new LinkedList<>(Arrays.asList("A","B","C"));
+//        list.forEach(str -> System.out.println(str));
+
+
+//        Iterator<String> iterator = list.iterator();
+//
+//        while(iterator.hasNext()){
+//            System.out.println((String) iterator.next());
+//        }
+
+//        System.out.println("Hello world!");
+//
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add(0, "AAA");
+//        list.add(0, "BBB");
+//        System.out.println(list);
+
+//        List<String> list = new ArrayList<>();
+//        list.add("AAA");
+//        System.out.println(list);
+//        System.out.println(list.size());
+//        System.out.println(list.remove("AAA"));
+//        System.out.println(list);
+
+
+
+
+    }
+}
