ff3846b7014099f8eb7bb040ab60cfeb7e5b291d
LinJie9 <2983606672@qq.com>
2025-04-26 17:20:42 +0800

    学习了缓冲流的内容

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,41 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Main {
+    private static final List<Book> LIST = new LinkedList<>();
     public static void main(String[] args) {
 
+        //图书管理系统
+        Scanner scanner = new Scanner(System.in);
 
-        try(BufferedWriter stream = new BufferedWriter(new FileWriter("test.txt"))){
-            stream.write("Hello World");
-            stream.newLine();
-            stream.write("Hello World");
-            stream.flush();
-        }catch (IOException e){
-            e.printStackTrace();
+        while (true){
+            System.out.println("========== 图书管理系统 ==========");
+            System.out.println("1. 录入书籍信息");
+            System.out.println("2. 查阅书籍信息");
+            System.out.println("3. 删除书籍信息");
+            System.out.println("3. 修改书籍信息");
+            System.out.println("5. 退出系统");
+            System.out.println("========== <END> ==========");
+
+            switch (scanner.nextInt()){
+                case 1:
+                    insert(scanner);
+                    break;
+                case 5:
+                    System.out.println("退出使用");
+                    break;
+            }
+
         }
 
-//        Set<Integer> set = new TreeSet<>((a, b) -> b - a); //比较，从大到小
-//        set.add(1);
-//        set.add(2);
-//        set.add(3);
-//        for (Integer i : set){
-//            System.out.println(i);
-//        }
+    }
 
-//            Set<String> set = new LinkedHashSet<>(); //顺序排序
-//            set.add("AAA");
-//            set.add("CCC");
-//            set.add("DDD");
-//            for (String s : set){
-//                System.out.println(s);
-//            }
+    private static void insert(Scanner scanner) {
 
+        String title = scanner.nextLine();
+        String author = scanner.nextLine();
+        int price = scanner.nextInt();
 
-//        LinkedList<String> list = new LinkedList<>(Arrays.asList("A","B","C"));
-//        list.forEach(str -> System.out.println(str));
-
-
-//        Iterator<String> iterator = list.iterator();
-//
-//        while(iterator.hasNext()){
-//            System.out.println((String) iterator.next());
-//        }
-
-//        System.out.println("Hello world!");
-//
-//        ArrayList<String> list = new ArrayList<>();
-//        list.add(0, "AAA");
-//        list.add(0, "BBB");
-//        System.out.println(list);
-
-//        List<String> list = new ArrayList<>();
-//        list.add("AAA");
-//        System.out.println(list);
-//        System.out.println(list.size());
-//        System.out.println(list.remove("AAA"));
-//        System.out.println(list);
-
-
-
-
+        Book book = new Book(title, author, price);
     }
 }
